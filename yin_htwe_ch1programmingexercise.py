0792cead44ec06c0672f6db35723c992085c67a1
Bt-PplusK <mgphoekitt24@gmail.com>
2025-12-17 17:15:42 +0630

    Assignments

---
+++ yin_htwe_ch1programmingexercise.py
@@ -0,0 +1,35 @@
+print ("Hello, world! ")
+#Output : Hello, world!
+
+print ("Hello", "world!")
+#Output : Hello world!
+
+print (3) 
+#Output : 3
+
+print (3.0)
+#Output : 3.0
+
+print (2 + 3) 
+#Output : 5
+
+print (2.0 + 3.0) 
+#Output : 5.0
+
+print ("2" + "3") 
+#Output : 23
+
+print ("2 + 3 =", 2 + 3)
+#Output : 2 + 3 = 5
+       
+print (2 * 3)
+#Output : 6
+
+print (2 ** 3)
+#Output : 8
+
+print (7 / 3)
+#Output : 2.3333333333333335
+
+print (7 //  3)
+#Output : 2
